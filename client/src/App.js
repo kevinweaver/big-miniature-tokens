00f09cf76cedfd0454184cfe47594ebc15d432dc
Kevin Weaver <kevinweaver2@gmail.com>
2021-03-21 01:01:38 -0400

    Add ReactPlayer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
 import axios from 'axios'
 
 import './App.css';
@@ -11,43 +12,48 @@ class ArtPage extends Component {
     }
   }
 
+  //https://ipfs.io/ipfs/QmNi8aGhTgSAxVCK7sLB9qHdVAyASkhJPn5FsouVKWYVjP/metadata.json
+
   getToken() {
     axios.get('/api/v1/tokens?artist_name=brogli')
       .then(response => {
         this.setState({token: response.data})
-        console.log(response.data.token)
+        console.log("axios", response.data.token)
       })
       .catch(error => console.log(error))
   }
 
   componentDidMount() {
-    this.getToken()
   }
 
   render() {
     return (
       <div>
         <div className="background">
-          <div className="art-image" />
+          <div className="nft-wrapper">
+            <ReactPlayer className="nft" url='./cthulhu.mp4' playing={true} loop={true} />
+          </div>
         </div>
-        <div className="info-container">
-          <div className="art-header">Cthulu</div>
-          <div className="collectors-edition">Collector's Edition 1/1 NFT</div>
-          <div className="gif">
+        <div className="content">
+          <div className="info-container">
+            <div className="art-header">Cthulhu</div>
+            <div className="collectors-edition">Collector's Edition 1/1 NFT</div>
+            <div className="wip">
 
-          </div>
-          <div className="artist-message-title">Artist Message</div>
-          <div className="artist-message">
-            Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
-            If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
-          </div>
-          <div className="nft-message-title">NFT Owner Message</div>
-          <div className="nft-owner-message">
-            Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
-          </div>
-          <div className="qr-message-title">QR Model Community</div>
-          <div className="qr-message">
-            If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+            </div>
+            <div className="artist-message-title">Artist Message</div>
+            <div className="artist-message">
+              Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
+              If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+            </div>
+            <div className="nft-message-title">NFT Owner Message</div>
+            <div className="nft-owner-message">
+              Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
+            </div>
+            <div className="qr-message-title"> Community</div>
+            <div className="qr-message">
+              If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+            </div>
           </div>
         </div>
       </div>
