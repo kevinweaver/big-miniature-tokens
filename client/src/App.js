835ca686890e207c197e1491fcdaf42d7d49e7df
Kevin Weaver <kevinweaver2@gmail.com>
2021-03-21 10:52:52 -0400

    update styling

--- client/src/App.js
+++ client/src/App.js
@@ -44,19 +44,49 @@ class ArtPage extends Component {
               <h2 className="collectors-edition">
                 Collector's Edition 1/1 NFT
               </h2>
-              <img className="wip" src={"./showcase.gif"} alt="showcase"/>
               <h2 className="artist-message-title">Artist Message</h2>
-              <div className="artist-message">
-                Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
-                If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+              <div className="artist-container">
+
+                <p className="artist-message">
+                  Thank you for supporting my work. As a digital sculptor and artist, I’ve always been fascinated by the line where the digital and physical world meet, and by creating this piece as both a physical model as well as a Non-Fungible Artwork Token through the Foundation App, this has become an artwork which lives on that line. As an extra thank-you to the owner of the NFT, you may redeem ownership of the Collector’s Edition 1/1 8” 3D-Printed resin model from my partners at Big Miniature World for free. This version includes a unique QR code with exclusive access to extra secret goods. Click the sign-in link at the bottom of the page to confirm NFT ownership and redeem your free Collectors Edition 1/1 Cthulhu Model.
+                  If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+                </p>
+                <img className="artist-progress" src={"./showcase.gif"} alt="showcase"/>
               </div>
               <h2 className="nft-message-title">NFT Owner Message</h2>
               <p className="nft-owner-message">
                 I am the captain now.
               </p>
-              <h2 className="qr-message-title"> Community</h2>
-              <div className="qr-message">
-                If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to Big Miniature World so they can display your work here!
+              <h2 className="qr-message-title">Community</h2>
+              <p className="qr-message">
+				      	If you bought the standard edition Cthulhu Rises model from Big Miniature world, this page is for you too. I encourage you to send your best photo of your painted Cthulhu Rises model to 
+               <a href="mailto:bigminiatureworld@gmail.com" target="_blank">
+                 Big Miniature World so they can display your work here!
+               </a>
+              </p>
+              <div className="gallery">
+
+              <div className="gallery-item">
+		  			  	<img src="FPO1.jpeg" alt="Cthulhu-Fanart" width="600" height="400"/>
+		  		  	  <div class="desc">Add a description of the image here</div>
+			    		</div>
+
+    					<div className="gallery-item">
+    						<img src="FPO2.jpeg" alt="Cthulhu-Fanart" width="600" height="400"/>
+    					  <div className="desc">Add a description of the image here</div>
+    					</div>
+
+    					<div className="gallery-item">
+    						<img src="FPO3.jpeg" alt="Cthulhu-Fanart" width="600" height="400"/>
+    					  <div className="desc">Add a description of the image here</div>
+    					</div>
+
+    					<div className="gallery-item">
+    					  <a href="#">
+    						<img src="FPO4.jpeg" alt="Cthulhu-Fanart" width="600" height="400"/>
+    					  </a>
+    					  <div className="desc">Add a description of the image here</div>
+    					</div>
               </div>
             </div>
           </div>
